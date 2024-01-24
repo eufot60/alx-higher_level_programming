519fad436506a4550f9372e80248beab7f61279d
eufot60 <eufot60@gmail.com>
2024-01-24 15:39:29 -0800

    h

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,6 @@
 def square_matrix_simple(matrix=[]):
     squared = []
     for line in matrix:
-        squared.append([c**2 for c in line])
+        squared.append([ c**2 for c in line ])
     return squared
-
 
