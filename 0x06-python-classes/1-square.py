8fe4426158e172c5a59fb6e9c53b6cb2e5b0365f
eufot60 <eufot60@gmail.com>
2024-02-06 16:02:34 -0800

    f

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 
+
 """Define a class Square."""
+
 
 
 class Sqaure:
     """Represent a square."""
 
+
     def __init__(self, size):
         """Initialize a new Square.
+
 
         Args:
             size (int): The size of the new square.
         """
+        
         self. __size = size
