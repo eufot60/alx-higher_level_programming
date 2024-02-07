675fee960eb20ec15787642c1b3b5999f9fb30c6
eufot60 <eufot60@gmail.com>
2024-02-06 16:17:06 -0800

    f

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -16,7 +16,7 @@ class Square:
 
     @property
     def size(self):
-        """Get/set the current size of the suare."""
+        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
