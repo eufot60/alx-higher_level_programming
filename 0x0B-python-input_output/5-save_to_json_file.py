760238b87882b55d0c31cefea4db7abba16920f5
eufot60 <eufot60@gmail.com>
2024-02-20 16:56:47 -0800

    g

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
-# 6-from_json_string.py
-"""Defines a JSON-to-object function."""
+#!/usr/bin/pythoni3
+"""Defines a JSON file-writing function."""
 import json
 
 
-def from_json_string(my_str):
-    """Return the python3 object represantation of a JSON string."""
-    return json.loads(my_str)
+def save_to_json_file(my_obj, filename):
+    """Write an object to a text file using JSON representation."""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
