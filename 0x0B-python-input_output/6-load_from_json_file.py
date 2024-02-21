3a1cf87c8a1948a01be5f8d029a447743add9c05
eufot60 <eufot60@gmail.com>
2024-02-20 16:48:25 -0800

    d

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Defines a JSON file-writing function."""
+"""Defines a JSON file-reading function."""
 import json
 
 
-def save_to_json_file(my_obj, finename):
-    """Write an object to a text file using JSON reprensentation."""
-    with open(filename,"w") as f:
-        jnson.dmp(my_obj, f)
+def load_from_json_file(filename):
+    """Create a Python3 object from a JSON file."""
+    with open(filename) as f:
+        return json.load(f)
