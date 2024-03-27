0689b5fde93c4f9a7f74a4a9785f12af1855bfec
eufot60 <eufot60@gmail.com>
2024-03-27 05:37:07 -0700

    good

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
---creates the MySQL server user user_0d_1 and grant all privileges
+--creates the MySQL server user user_0d_1 and grant all priviledges
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_od_1_pwd';
 GRANT ALL PRIVILEDGES ON * . * TO user_0d_1@localhost;
