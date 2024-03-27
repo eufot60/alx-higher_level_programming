b59348cb223d663f8e18c70e2bb4fb07b4e1f958
eufot60 <eufot60@gmail.com>
2024-03-26 18:40:19 -0700

    good

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- Removes all records with a score <= 5 in the table second_table in my MySQL server.
+FROM `second_table`
+`score` <= 5;
---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,5 @@
+-- Creates and fills a table second_table with attributes id, name and score in my MySQL server with multiple rows.
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10); 
+INSERT INTO `second_table` (`id`, `name`, `score`) ValUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) ValUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) ValUES (4, "George", 8);
