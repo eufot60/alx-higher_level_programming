2f86ed56da8ec891fd29ab39b6d10017158aded0
eufot60 <eufot60@gmail.com>
2024-04-22 13:56:07 -0700

    g

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const arglength = process.argv.length;
+
+if (argLength === 2) {
+	console.log('No argument');
+}	else if (argsLength ===3) {
+	console.log('Argument found');
+}       else {
+	console.log('Argument found');
+}
