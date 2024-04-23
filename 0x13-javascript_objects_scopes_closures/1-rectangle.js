1113f41fabdc55686e0d6d4d656364e0c1214f5c
eufot60 <eufot60@gmail.com>
2024-04-23 10:55:48 -0700

    g

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-// Write a class Rectangle that defines a rectangle:
+// Write a class Rectangle that defines a rectangle class with width and height attributes.
 
 module.exports = class Rectangle {
 	constructor (width, height) {
-		this.width = width;
-		this.height = height;
+	  this.width = width;
+	  this.height = height;
 	}
 };
