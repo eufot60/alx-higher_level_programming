e435f2e13a08b2e03a3865ff977bc47ecfab12c1
eufot60 <eufot60@gmail.com>
2024-04-23 16:58:48 -0700

    g

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  
+  print () {
+    const array = [];
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+	array.push('X');
+      }
+      console.log(array.join(''));
+      array.length = 0;
+    }
+  }
+  roatate (){
+   const tmp = this.height;
+   this.height = this.width;
+   this.width = tmp;
+  }
+
+  double () {this.height *= 2;
+	  this.width *= 2;
+  }
+
+};
