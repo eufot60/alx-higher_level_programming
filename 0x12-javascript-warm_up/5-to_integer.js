a38d879a44ba2f9619eb55a4bd4c09fc801a43c9
eufot60 <eufot60@gmail.com>
2024-04-22 18:01:35 -0700

    good

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const arg2 = process.argv[2];
-const parsedNumber = parseInt(arg2);
+const parseNumber = parseInt(arg2);
 
-if (isNAN(parsedNumber)) {
+if (isNAN(parseNumber)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ${parsedNumber}');
+  console.log('My number: ${parseNumber}');
 }
