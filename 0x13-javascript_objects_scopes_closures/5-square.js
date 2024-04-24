8aa0902951a053e35eaf8175ac22c078ef5ef0ea
eufot60 <eufot60@gmail.com>
2024-04-23 19:47:34 -0700

    d

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,10 +2,14 @@
 
 // Creates a squsre class that extends Rectangle class
 
-const Rectangle = require('./4-rectangle.js');
+const Rectangle = require('./4-rectangle');
 
 module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
+  }
+
+  double () {
+    super.double();
   }
 };
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,18 +2,23 @@
 
 //Create an instance method called charPrint(c) that prints the rectangle using the character c
 
-const OldSquare = require('./5-square');
+const Rectangle = require('./5-square.js');
 
-module.exports = class Square extends OldSquare {
+module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
 
-  double () {
-    super.double();
-  }
-
-  charPrint (c = 'X') {
-    super.print(c);
+  charPrint (c) {
+    if (c === undefined) { c = 'X'; }
+    const array = [];
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.width; j++) {
+        array.push(c);
+      }
+      console.log(array.join(''));
+      array.length = 0;
+    }
   }
 };
+
