5f494d1dbdf99169e0ef4511fb877f983fe5b716
eufot60 <eufot60@gmail.com>
2024-04-23 20:19:03 -0700

    g

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,7 +2,9 @@
 
 // Creates a squsre class that extends Rectangle class
 
-module.exports = class Square extends require('./4-rectangle.js') {
+const Rectangle = require('./4-rectangle.js');
+
+module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
