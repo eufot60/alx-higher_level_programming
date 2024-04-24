ab049e6a48ae1ada4c08867172718920426885dc
eufot60 <eufot60@gmail.com>
2024-04-23 19:56:39 -0700

    g

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Write a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.
+
+const dict = require('./101-data').dict;
+const newDict = {};
+
+Object.keys(dict).map(function (key) {
+  if (!Array.isArray(newDict[dict[key]])) {
+    newDict[dict[key]] = [];
+  }
+  newDict[dict[key]].push(key);
+});
+
+console.log(newDict);
