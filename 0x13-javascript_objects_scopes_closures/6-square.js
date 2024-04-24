9c6ac217e44ef7b7f8e64da8ce262119e035f171
eufot60 <eufot60@gmail.com>
2024-04-23 19:25:39 -0700

    g

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+//Create an instance method called charPrint(c) that prints the rectangle using the character c
+
+const OldSquare = require('./5-square');
+
+module.exports = class Square extends OldSquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  double () {
+    super.double();
+  }
+
+  charPrint (c = 'X') {
+    super.print(c);
+  }
+};
