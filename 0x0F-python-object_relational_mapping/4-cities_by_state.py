b53af44fb40007406af5c4096159b05005189212
eufot60 <eufot60@gmail.com>
2024-04-28 07:46:01 -0700

    g

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -3,8 +3,8 @@
 Write a script that lists all cities from the database hbtn_0e_4_usa
 """"
 
-import MySQLdb
 import sys
+import MySQLdb
 
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
