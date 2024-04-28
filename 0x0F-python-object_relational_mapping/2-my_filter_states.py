fba8f50a078113d49a2647577a85ab92386fc176
eufot60 <eufot60@gmail.com>
2024-04-28 07:23:44 -0700

    g

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,25 +4,16 @@
 import MySQLdb
 import sys
 
-
 if __name__ -- '__main__':
-
-    db = MSQLdb.connect(
-        host='localhost',
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-        )
-    state_name = sys.argv[4]
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '%{}%'\
-    ORDER BY states.id ASC".format(state_name))
-    table = cur.fetchall()
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
+    states = cur.fetchall()
 
-    for row in table:
-        print(row)
-
-    cur.close()
-    db.close()
+    for state in states:
+        print(state)
