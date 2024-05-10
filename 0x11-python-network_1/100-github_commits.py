878b643fd2f9239cd497bbf6242f7a32d51895f6
eufot60 <eufot60@gmail.com>
2024-05-10 05:34:08 -0700

    t

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,27 +2,19 @@
 """
 Please list 10 commits (from the most recent to oldest) of the repository “rails” by the user “rails”
 """
+from sys import argv
+import requests
 
 
 if __name__ == '__main__':
-    from requests import get
-    from sys import argv
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    req = requests.get(url)
+    commits = req.json()
 
-    repo = argv[1]
-    owner = argv[2]
-    i = 0
-
-    URL = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
-
-    response = get(URL)
-    json = response.json()
-
-    for element in json:
-        if i > 9:
-            break
-        sha = element.get('sha')
-        author = element.get('commit').get('author').get('name')
-        print("{}: {}".format(sha, author))
-        i += 1
-
-
+    try:
+        for index in range(10):
+            print("{}: {}".format(
+                commits[index].get("sha"),
+                commits[index].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
