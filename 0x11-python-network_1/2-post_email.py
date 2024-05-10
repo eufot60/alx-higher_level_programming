b5d09435ff3ebed6ae4a5840ffc013cbdc5543d0
eufot60 <eufot60@gmail.com>
2024-05-10 10:28:21 -0700

    d

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """a Python script that takes in a URL and an email, sends a POST request tothe passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)"""
 
-if __name__ == '__main__':
-    import urllib.request
-    import urllib.parse
-    import sys
+import sys
+import urllib.parse
+import urllib.request
 
-    argv = sys.argv
-    url = argv[1]
-    email = argv[2]
-    DATA = urllib.parse.urlencode({"email": email})
-    DATA = DATA.encode('ascii')
 
-    with urllib.request.urlopen(url, DATA) as response:
-        print(response.read().decode('utf-8'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
