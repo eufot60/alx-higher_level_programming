cb30baea1303485415335b0808b187d5c36cec3d
eufot60 <eufot60@gmail.com>
2024-05-10 05:17:15 -0700

    q

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,20 +4,24 @@ Please list 10 commits (from the most recent to oldest) of the repository “rai
 You must use the GitHub API, here is the documentation https://developer.github.com/v3/repos/commits/
 Print all commits by: `<sha>: <author name>` (one by line)
 """
-import sys
-import requests
 
+if _name__ == '__main__':
+    from requests import get
+    from sys import argv
 
-if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
+     repo = argv[1]
+    owner = argv[2]
+    i = 0
 
-    r = requests.get(url)
-    commits = r.json()
-    try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+    URL = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+
+    response = get(URL)
+    json = response.json()
+
+    for element in json:
+        if i > 9:
+            break
+        sha = element.get('sha')
+        author = element.get('commit').get('author').get('name')
+        print("{}: {}".format(sha, author))
+        i += 1
